dae2813dc1a832a948a3f0ba81e3e77ec6199f93
RitikKShahi <ritikshahi071@gmail.com>
2026-01-14 21:13:01 +0530

    completed the TCP layer

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -2,12 +2,18 @@ package p2p
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTCPTransport(t *testing.T){
-	listenAddr := ":4000"
-	tr := NewTCPTransport(listenAddr)
-	assert.Equal(t,tr.listenAddress,listenAddr)
-	assert.Nil(t,tr.ListenAndAccept())
+func TestTCPTransport(t *testing.T) {
+	opts := TCPTransportOpts{
+		ListenAddr:    ":3000",
+		HandshakeFunc: NOPHandshakeFunc,
+		Decoder:       DefaultDecoder{},
+	}
+	tr := NewTCPTransport(opts)
+	assert.Equal(t, tr.ListenAddr, ":3000")
+
+	assert.Nil(t, tr.ListenAndAccept())
 }
